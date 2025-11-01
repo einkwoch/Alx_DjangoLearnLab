ec13da83a23d510be8ef798ab475fa5e49efb196
Emmanuel <emmanuelizu92@gmail.com>
2025-11-01 01:15:54 -0700

    Introduction to Django Development Environment Setup

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ycs5+v2%4olf_&*%zm&69oge=)liedwa=3zy6i8@u7j%t3d26#'
+SECRET_KEY = 'django-insecure-bo7)7m7k$xk%uts+bwp-2o+_tn4=93&#pg)_e+s&w3zr08g0wf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
