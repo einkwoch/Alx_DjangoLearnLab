a49d03db819ef726d3743c8d59c30b8ddcb3abb3
Emmanuel <emmanuelizu92@gmail.com>
2025-12-02 23:11:03 -0800

    Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('posts/', views.ListBlogPost.as_view(), name = 'posts'),
     path('posts/<int:pk>/', views.DetailBlogPost.as_view(), name = 'detail'),
-    path('posts/new/', views.CreatBlogPost.as_view(), name = 'create'),
-    path('posts/<int:pk>/edit/', views.UpdateBlogPost.as_view(), name = 'update'),
-    path('posts/<int:pk>/delete/', views.DeleteBlogPost.as_view(), name = 'delete'),
+    path('post/new/', views.CreatBlogPost.as_view(), name = 'create'),
+    path('post/<int:pk>/update/', views.UpdateBlogPost.as_view(), name = 'update'),
+    path('post/<int:pk>/delete/', views.DeleteBlogPost.as_view(), name = 'delete'),
 ]
